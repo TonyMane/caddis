07c2b2099496af10989eb1b0b647bcf82b4f6576
Anthony Bertagnolli <32308413+TonyMane@users.noreply.github.com>
2021-06-06 14:57:39 -0600

    Update genericRcommands.R

--- genericRcommands.R
+++ genericRcommands.R
@@ -1,5 +1,6 @@
 #commands for converting dada2-like ASV/sample files to ASV/sample text file and ASV fasta file.
 #taken from https://github.com/benjjneb/dada2/issues/48, comment by jeffkimbrel (go Beavs!).
+library(phyloseq)
 seqs <- colnames(seqtab)
 otab <- otu_table(seqtab, taxa_are_rows=FALSE)
 colnames(otab) <- paste0("seq", seq(ncol(otab)))
