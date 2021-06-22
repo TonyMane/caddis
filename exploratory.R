82ad8118185bdd3af6f0c0b719bd0adcd7d5dca7
Anthony Bertagnolli <32308413+TonyMane@users.noreply.github.com>
2021-06-22 01:37:20 -0600

    Update exploratory.R

--- exploratory.R
+++ exploratory.R
@@ -1,3 +1,9 @@
+#for looking subset data, n=5 samples per group (fly, net, retreat, swab)
 NAMES<-length(caddis_meta$Sample_NAME[c(1:5, 30:34, 50:54, 80:84)])
 
-ps3<-transform_sample_counts(ps2, function(OTU) OTU/sum(OTU) * 100)
+#produce relative abundance matrix
+ps_Phylum_rel<-transform_sample_counts(ps_Phylum, function(OTU) OTU/sum(OTU)*100)
+
+#collapse matrix by Genus abundance                                       
+ps_Genus<-tax_glom(ps, "Genus", NArm=TRUE)
+                                       
