2e0b295c2e371442758823dff53c5f7bf4546da9
Anthony Bertagnolli <32308413+TonyMane@users.noreply.github.com>
2021-06-22 02:11:41 -0600

    Update exploratory.R

--- exploratory.R
+++ exploratory.R
@@ -1,6 +1,12 @@
 #for looking subset data, n=5 samples per group (fly, net, retreat, swab)
 NAMES<-length(caddis_meta$Sample_NAME[c(1:5, 30:34, 50:54, 80:84)])
 
+library(phyloseq)
+library(tidyverse)
+ps <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), 
+               sample_data(samdf), 
+               tax_table(taxa))
+
 #produce relative abundance matrix
 ps_Phylum_rel<-transform_sample_counts(ps_Phylum, function(OTU) OTU/sum(OTU)*100)
 
