bc4ad279f5cbcdbfaee526d04a203ab6f1299fce
Anthony Bertagnolli <32308413+TonyMane@users.noreply.github.com>
2021-06-22 01:43:13 -0600

    Update exploratory.R

--- exploratory.R
+++ exploratory.R
@@ -7,3 +7,8 @@ ps_Phylum_rel<-transform_sample_counts(ps_Phylum, function(OTU) OTU/sum(OTU)*100
 #collapse matrix by Genus abundance                                       
 ps_Genus<-tax_glom(ps, "Genus", NArm=TRUE)
                                        
+ps_genus_rel= transform_sample_counts(ps_Genus, function(x) x / sum(x) )
+Desulfobacterota <- subset_taxa(ps_genus_rel, Phylum =="Desulfobacterota")
+Desulfobacterota_little<-prune_samples(NAMES, Desulfobacterota)
+Desulfobacterota_2<-filter_taxa(Desulfobacterota_little, function(x) sum(x) >0.01, TRUE)
+plot_bar(Desulfobacterota_2, fill="Genus")
