1701344ee232f9303f885edbe71a78b633793533
Anthony Bertagnolli <32308413+TonyMane@users.noreply.github.com>
2021-06-25 22:17:29 -0600

    Update genericRcommands.R

--- genericRcommands.R
+++ genericRcommands.R
@@ -14,5 +14,16 @@ write.table(otab, "dada_table.txt",quote=FALSE,sep="\t")
 #To make a phylogenetic tree in a phyloseq obect.
 random_tree = rtree(ntaxa(physeq), rooted=TRUE, tip.label=taxa_names(physeq))
 
-ordu = ordinate(ps, "PCoA", "unifrac", weighted=TRUE)
+#add the tree to a physeq object
+physeq1 = merge_phyloseq(ps, caddis_meta, random_tree)
 
+#rename to original object
+ps<-physeq1
+
+#perform an ordination
+ordu = ordinate(ps, "PCoA", "unifrac", weighted=TRUE)
+#plot the result
+plot_ordination(ps, ordu, color="Type")
+
+#evaluate differential contributions of taxa that are statisitically significant
+ps.prop <- transform_sample_counts(ps, function(otu) otu/sum(otu))
