d9b0bbca42a3b87d8521b710bab0a5589bb30c23
Anthony Bertagnolli <32308413+TonyMane@users.noreply.github.com>
2021-06-25 21:59:27 -0600

    Update genericRcommands.R

--- genericRcommands.R
+++ genericRcommands.R
@@ -10,3 +10,7 @@ write.table(otab, "dada_table.txt",quote=FALSE,sep="\t")
 
 #final command for producing fasta file using bash
 #grep -v '^x' dada_seqs.txt | awk '{print ">seq"$1"\n"$2}' > dada_seqs.fa; rm dada_seqs.txt
+
+#To make a phylogenetic tree in a phyloseq obect.
+random_tree = rtree(ntaxa(physeq), rooted=TRUE, tip.label=taxa_names(physeq))
+
